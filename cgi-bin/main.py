18bafcee89957509116a0e24796039171f06c824
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-25 10:47:38 +0800

    add

--- cgi-bin/main.py
+++ cgi-bin/main.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 #coding:utf-8
 import modules.get_weather as get_weather
 import os,time,cgi,hashlib
-import sys
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
 indexDick={}
 def md5(alpha):
     return hashlib.md5(alpha.encode('utf-8')).hexdigest()
