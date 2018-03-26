1618ebd2e69002d3d37cc48cad0124755aa28641
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-26 17:26:19 +0800

    add security port script

--- cgi-bin/sp.py
+++ cgi-bin/sp.py
@@ -2,34 +2,39 @@
 #coding:utf-8
 import cgi,os,subprocess,re
 
+print("Content-type:text/html")
+print()
 data=cgi.FieldStorage()
+
 sp={}
 sp['port']=data.getvalue('port')
 sp['ip']=data.getvalue('ip')
 sp['prot']=data.getvalue('prot')
-iptablesGrepNum='sudo iptables -L INPUT -n --line-number|grep {ip}|grep {port}|grep {prot}|wc -l'.format_map(sp)
-iptablesGrep='sudo iptables -L INPUT -n --line-number|grep {ip}|grep {port}|grep {prot}'.format_map(sp)
+if sp['port'] and sp['ip'] and sp['prot']:
+    pass
+else:
+    print('you should specify key and value.')
+    exit()
+iptablesGrepNum='sudo iptables -L INPUT -n --line-number|grep :{port}$|grep {prot}|grep ACCEPT|wc -l'.format_map(sp)
+iptablesGrep='sudo iptables -L INPUT -n --line-number|grep :{port}$|grep {prot}|grep ACCEPT'.format_map(sp)
 iptablesInsert='sudo iptables -I INPUT -p {prot} --dport {port} -s {ip} -j ACCEPT'.format_map(sp)
 
-grepNumResult=subprocess.getoutput(iptablesGrepNum)
-grepResult=subprocess.getoutput(iptablesGrep).split('\n')
-
-while subprocess.getoutput(iptablesGrepNum) != 0:
+while str(subprocess.getoutput(iptablesGrepNum)) != str(0):
     grepResult = subprocess.getoutput(iptablesGrep).split('\n')[0]
-    sp['num'] = re.findall(r'(\d+)\s+ACCEPT', grepResult)[0]
-    subprocess.call('sudo iptables -D INPUT {num}'.format_map(sp), shell=True)
-
+    try:
+        sp['num'] = re.findall(r'(\d+)\s+ACCEPT', grepResult)[0]
+        subprocess.call('sudo iptables -D INPUT {num}'.format_map(sp),shell=True)
+    except Exception as e:
+        print(e)
+        exit()
 try:
     result = subprocess.call(iptablesInsert,shell=True)
 except Exception as e:
-    result=1
-
-
-
-
-print("Content-type:text/html")
-print()
-if result == 0:
-    print('Insertion Success')
-else:
-    print('Insertion Failed')
+    print(e)
+    exit()
+finally:
+    if result == 0:
+        print('Insertion Success')
+    else:
+        print('Insertion Failed</br>')
+        print(subprocess.getoutput(iptablesInsert))
