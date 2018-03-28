d8b160d26834a84fdc9bb75b45726dbafaac1c86
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-28 10:24:28 +0800

    update

--- cgi-bin/updateSophiroth.py
+++ cgi-bin/updateSophiroth.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 #-*-coding:utf-8-*-
 import subprocess
-subprocess.call('/home/alvin/bin/syncsophiroth.py',shell=True)
+print("Content-type:text/html")
+print()
+
+a=subprocess.call('/home/alvin/bin/syncsophiroth.py',shell=True)
+if str(a) == '0':
+    print('update success')
+else:
+    print('update may not success')
