0e1d0e84a1fea5aaec2ceb6cda4e3689e4f4c17e
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-04-02 14:54:02 +0800

    添加了天弘基金收益模拟界面。

--- js/main_apps.js
+++ js/main_apps.js
@@ -6,7 +6,7 @@ function apps() {
     document.write('<p><a href="/html/security_port.html" target="_blank"> 添加安全端口信息。</a> </p>');
     document.write('<p><a href="/html/change_vpn.html" target="_blank"> 修改vpn类型。</a> </p>');
     document.write('<p><a href="/cgi-bin/updateSophiroth.py" target="_blank"> 更新线上的代码。</a> </p>');
-
+    document.write('<p><a href="/cgi-bin/queryTianhong.py" target="_blank"> 天弘基金收益模拟。</a> </p>');
 
 }
 apps()
